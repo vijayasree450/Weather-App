aff2ae8f78c34a15bac4c0b6477f79054117b242
vijayasree450 <vijayasree450@gmail.com>
2026-02-13 11:26:57 +0530

    Update home file

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget {
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isDayTime = false;
+  @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
         onPressed: (){}, 
         icon:  const  Icon(Icons.more_vert)),
+      
+      actions:[
+        Switch(
+          value: isDayTime, 
+        onChanged: (value){
+          setState(() {
+            isDayTime = value;
+          });
+        })
+      ]
       ),
     );
   }
