455ca292c1403ea5b0bb41415e2e2e231bacdf49
vijayasree450 <vijayasree450@gmail.com>
2026-02-13 11:43:48 +0530

    Update home file

--- lib/home.dart
+++ lib/home.dart
@@ -19,14 +19,51 @@ class _HomeState extends State<Home> {
         icon:  const  Icon(Icons.more_vert)),
       
       actions:[
-        Switch(
-          value: isDayTime, 
-        onChanged: (value){
-          setState(() {
-            isDayTime = value;
-          });
-        })
-      ]
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Switch(
+            activeColor: Colors.black,
+            inactiveThumbColor: Colors.black,
+            thumbColor: const MaterialStatePropertyAll(Colors.orange),
+            inactiveTrackColor: Colors.transparent,
+            thumbIcon: MaterialStatePropertyAll(
+              Icon( 
+                isDayTime 
+                ? Icons.wb_sunny : 
+                Icons.nightlight_round,
+                color: Colors.black,)
+            ),
+            value: isDayTime, 
+          onChanged: (value){
+            setState(() {
+              isDayTime = value;
+            });
+          }),
+        ),
+      ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+        child: Column(
+          children: [
+            Text(
+              "Attenganam",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+              ),
+              
+              
+              ),
+            Icon(
+              Icons.wb_sunny,
+               size: 250, 
+               color: Colors.orange)
+          ],
+
+        ),
+      ),
       ),
     );
   }
